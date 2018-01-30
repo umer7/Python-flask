8de2521fb0332e581b101736e079a46ff9d7752c
Umer Farooq <k142229@nu.edu.pk>
2018-01-30 16:58:26 +0500

    Update 23jan.py

--- 23jan.py
+++ 23jan.py
@@ -34,17 +34,17 @@ def use():
 def qa(question):
     #print('%s'%question)
     # show the user profile for that user
-    try:
-        parse.uses_netloc.append("postgres")
-        url = parse.urlparse(os.environ["DATABASE_URL"])
+    
+    parse.uses_netloc.append("postgres")
+    url = parse.urlparse(os.environ["DATABASE_URL"])
 
-        conn = psycopg2.connect( database=url.path[1:],    user=url.username,    password=url.password, host=url.hostname, port=url.port )
-        cur = conn.cursor()
+    conn = psycopg2.connect( database=url.path[1:],    user=url.username,    password=url.password, host=url.hostname, port=url.port )
+    cur = conn.cursor()
         
-        cur.execute("INSERT INTO test2 (qa) VALUES (%s)", (str(question)))
-        print("sucessfull")
-    except:
-        print("failed")
+    cur.execute("INSERT INTO test2 (qa) VALUES (%s)", (str(question)))
+ #       print("sucessfull")
+  
+  
     return 'question is  %s' % question
 
 
