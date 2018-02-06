59280db61026c594a70a26ef51d46add59c8955f
Umer Farooq <k142229@nu.edu.pk>
2018-02-06 00:33:59 -0800

    Update 23jan.py

--- 23jan.py
+++ 23jan.py
@@ -66,5 +66,45 @@ def ans(answer):
  #       print("sucessfull")
     return 'answer is  %s' % answer
 
+
+def get_cosine(vec1, vec2):
+    vec1 = Counter(WORD.findall(vec1))
+    vec2 = Counter(WORD.findall(vec2))
+
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x] ** 2 for x in vec1.keys()])
+    sum2 = sum([vec2[x] ** 2 for x in vec2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+
+def DistJaccard(str1, str2):
+    str1 = set(str1.split())
+    str2 = set(str2.split())
+    return float(len(str1 & str2)) / len(str1 | str2)
+
+
+def text_to_vector(text):
+    words = WORD.findall(text)
+    return Counter(words)
+
+
+text1 = 'I like cat'
+text2 = 'I like dog'
+
+
+# Function to get the correct spelling
+def remove_spell_errors(text):
+    b = TextBlob(text)
+    return b.correct()
+
+
+
 if __name__ == '__main__':
     app.run()
